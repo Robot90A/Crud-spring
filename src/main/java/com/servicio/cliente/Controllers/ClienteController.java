4dcaa492c6f146f8c69bbd5dce2ca5bdde963dad
Robot90A <sebastianandres428@gmail.com>
2025-09-06 15:31:08 -0500

    se creo atualizo el panel de login

--- src/main/java/com/servicio/cliente/Controllers/ClienteController.java
+++ src/main/java/com/servicio/cliente/Controllers/ClienteController.java
@@ -1,25 +1,23 @@
 package com.servicio.cliente.Controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.servicio.cliente.Models.ClienteModels;
 import com.servicio.cliente.Service.ClienteServce;
+
 
 @Controller
 @RequestMapping(value = "/cliente")
 public class ClienteController {
 
-    //inyeccion de dependenia
+    
     @Autowired
     private ClienteServce clienteServce;
 
@@ -27,7 +25,6 @@ public class ClienteController {
     public String ClenteFormulario(Model modelo){
 
         ClienteModels cliente = new ClienteModels();
-
         modelo.addAttribute("miCliente", cliente);
 
         return "formulario-cliente";
@@ -39,7 +36,6 @@ public class ClienteController {
     public String listaCliente(Model modelo){
 
         List<ClienteModels> ListaClientea = clienteServce.ListarCliente();
-
         modelo.addAttribute("ListaClientes", ListaClientea);
 
         return "lista-cliente";
@@ -61,7 +57,6 @@ public class ClienteController {
     public String CargarCliene(Model modelo, @RequestParam("id") Long idCliente){
 
         ClienteModels miClienteModels = clienteServce.CargarCliente(idCliente)
-
                .orElseThrow(() -> new RuntimeException("Cliente no encontrado"));
 
         modelo.addAttribute("miCliente", miClienteModels);
