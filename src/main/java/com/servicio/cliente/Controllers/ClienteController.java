56e811dbb58ba8010991f896ff65ee03a27ebbf5
Robot90A <sebastianandres428@gmail.com>
2025-09-06 11:23:05 -0500

    se agrego el html de la lista con su respectivo css, , se creo los endpoin de guardar y lista y se hicieron pruebas

--- src/main/java/com/servicio/cliente/Controllers/ClienteController.java
+++ src/main/java/com/servicio/cliente/Controllers/ClienteController.java
@@ -1,15 +1,24 @@
 package com.servicio.cliente.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.servicio.cliente.Models.ClienteModels;
+import com.servicio.cliente.Service.ClienteServce;
 
 @Controller
 @RequestMapping(value = "/cliente")
 public class ClienteController {
+
+
+    @Autowired
+    private ClienteServce clienteServce;
 
     @RequestMapping(value = "/Formulario", method = RequestMethod.GET)
     public String ClenteFormulario(Model modelo){
@@ -19,6 +28,29 @@ public class ClienteController {
         modelo.addAttribute("miCliente", cliente);
 
         return "formulario-cliente";
+    }
+
+
+
+    @RequestMapping("/Lista")
+    public String listaCliente(Model modelo){
+
+        List<ClienteModels> ListaClientea = clienteServce.ListarCliente();
+
+        modelo.addAttribute("ListaClientes", ListaClientea);
+
+        return "lista-cliente";
+    }
+
+    
+    @RequestMapping("/guardar")
+    public String GuardarCliente(@ModelAttribute("miCliente") ClienteModels cliente){
+
+        clienteServce.GuardarCliente(cliente);
+
+        return "redirect:/cliente/Lista";
+
+    
     }
 
 
